beda608b8ddf0ceddd77faa03a994cf278178310
iago.garcia.tc <iago.garcia.tc@fornecedores.klabin.com.br>
2024-01-27 15:28:54 -0300

    start spring boot project

---
+++ projetox/src/test/java/br/com/projetox/projetox/ProjetoxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetox.projetox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
