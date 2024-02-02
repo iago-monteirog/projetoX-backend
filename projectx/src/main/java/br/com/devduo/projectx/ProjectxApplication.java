88debc586f429c1020a25e42dfe8da5f384a3e86
iago-monteirog <iagomgarcia9000@gmail.com>
2024-02-01 21:25:43 -0300

    build: changes java version to 17 and add lombok and validation dependency

---
+++ projectx/src/main/java/br/com/devduo/projectx/ProjectxApplication.java
@@ -0,0 +1,13 @@
+package br.com.devduo.projectx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectxApplication.class, args);
+	}
+
+}
